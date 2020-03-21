c665f09ec9db6e9164c1bf0513642e03586a05f2
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-21 18:51:17 +0100

    Colleen in Python

---
+++ Python/Colleen.py
@@ -0,0 +1,9 @@
+#Bonus Colleen in Python
+
+def toto():
+    pass
+
+if __name__ == "__main__":
+    toto()
+    s="#Bonus Colleen in Python{0:c}{0:c}def toto():{0:c}    pass{0:c}{0:c}if __name__ == {1:c}__main__{1:c}:{0:c}    toto(){0:c}    s={1:c}{s}{1:c}{0:c}    print(s.format(10,34,s=s))"
+    print(s.format(10,34,s=s))
