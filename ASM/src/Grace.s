9775f7303776e1a9a6ec4df3cdf0f542de91038a
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-23 19:01:05 +0100

    Grace debuts

---
+++ ASM/src/Grace.s
@@ -0,0 +1,11 @@
+# make 3 defines
+ ->> char to print
+ ->> print function
+ ->> is a macro a define ?
+# make a macro for main
+ ->> need fopen, fprintf, fclose
+ ->> need to save return value of fopen
+ ->> if return value of fopen is 0, exit the programm
+ ->> else write in file
+ ->> close file
+# call main macro
